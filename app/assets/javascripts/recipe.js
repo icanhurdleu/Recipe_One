8387e2b28d9ef8daa04643948a08e733edfca261
Sam Ellis <icanhurdleu@gmail.com>
2020-08-31 19:23:36 -0400

    Add jQuery search feature

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
-	setTimeout(function(){
-		$('#flash-notice').remove();
-	}, 5000);
-})
+jQuery(document).ready(function() {
+	// setTimeout(function(){
+	// 	$('#flash-notice').remove();
+	// }, 5000);
+
+	// Check for click events on the navbar burger icon
+	$(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+
+  $('#search-box').keyup(function() {
+  	var matcher = new RegExp($(this).val(), 'gi');
+  	$('.recipe-holder').show().not(function() {
+  		return matcher.test($(this).find('.title').text());
+  	}).hide();
+  });
+
+});
