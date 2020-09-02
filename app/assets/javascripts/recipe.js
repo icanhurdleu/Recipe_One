819a42af160582fbc62b6bc85611af95a85f614b
Sam Ellis <icanhurdleu@gmail.com>
2020-09-01 21:30:55 -0400

    Fix nested form formatting

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+jQuery(document).on( "turbolinks:load", function() {
 	// setTimeout(function(){
 	// 	$('#flash-notice').remove();
 	// }, 5000);
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,9 @@
 class TagsController < ApplicationController
 
+	def index
+		@tag = Tag.all.order("name DESC")
+	end
+
 	def show
 		@tag = Tag.find(params[:id])
 	end
